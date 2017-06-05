a76d4feeebdfb7d618aa46015f05735f5bad6186
ChiLi_Lin <904981663@163.com>
2017-06-05 17:57:37 +0800

    Study PUN

---
+++ Assets/MyDemo/PUNConnect.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PUNConnect : MonoBehaviour
+{
+
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+        //连接同一个游戏的话，版本号要相同
+        PhotonNetwork.ConnectUsingSettings("1.0");
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width / 2 - 75, 20, 150, 20), PhotonNetwork.connectionStateDetailed.ToString());
+
+        if (GUI.Button(new Rect(Screen.width / 2 - 50, 50, 100, 30), "加入游戏房间"))
+            if (PhotonNetwork.connected)
+                PhotonNetwork.JoinOrCreateRoom("6666", new RoomOptions { maxPlayers = 4 }, null);
+
+        if (GUI.Button(new Rect(Screen.width / 2 - 50, 100, 100, 30), "离开游戏房间"))
+            if (PhotonNetwork.connected)
+                PhotonNetwork.LeaveRoom();
+    }
+
+}
---
+++ Assets/MyDemo/PlaneClick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneClick : MonoBehaviour 
+{
+    public GameObject prefabs;
+
+    void OnClick()
+    {
+        Debug.Log(InputToEvent.inputHitPos);
+        if(PhotonNetwork.connectionStateDetailed == ClientState.Joined)
+        {
+            PhotonNetwork.Instantiate(prefabs.name, InputToEvent.inputHitPos + new Vector3(0,3,0), Quaternion.identity, 0);
+        }
+
+    }
+
+
+}
